8cf07161f61dfb62aeff26104fbace5d38390e66
rubansi <rubansi24@gmail.com>
2025-08-02 19:34:05 +0300

    list data types in python and accessing items from a list

--- python-fm/wk-1/datatypes.py
+++ python-fm/wk-1/datatypes.py
@@ -7,3 +7,15 @@ print(num2)
 print(type(num1)) #int
 print(type(num2)) #float
 
+# list datatypes
+languages = ["Python", "Dart", "Web", 23]
+print(languages)
+
+# Access List Items
+
+# To access items from a list, we use the index number (0, 1, 2 ...). For example,
+
+print(languages[0]) #Python
+print(languages[1]) #Dart
+print(languages[2]) #Web
+print(languages[3]) #23
