504e86003a55d54b9d169b1533b8858dcd730311
rubansi <rubansi24@gmail.com>
2025-08-02 19:52:00 +0300

    set datatypes

---
+++ python-fm/wk-1/list datatypes.py
@@ -0,0 +1,21 @@
+num1 = 55
+num2 = 5.3
+print(num1)
+print(num2) 
+
+# checking the type of variable
+print(type(num1)) #int
+print(type(num2)) #float
+
+# list datatypes
+languages = ["Python", "Dart", "Web", 23]
+print(languages)
+
+# Access List Items
+
+# To access items from a list, we use the index number (0, 1, 2 ...). For example,
+
+print(languages[0]) #Python
+print(languages[1]) #Dart
+print(languages[2]) #Web
+print(languages[3]) #23
---
+++ python-fm/wk-1/set datatype.py
@@ -0,0 +1,9 @@
+# The Set is an unordered collection of unique items. Set is defined by values separated by commas inside braces { }. For example,
+
+student_ids = {112, 114, 117, 113}
+print(student_ids)
+
+
+
+# Here, we have created a set named student_info with 5 integer values.
+# Since sets are unordered collections, indexing has no meaning. Hence, the slicing operator [] does not work.
