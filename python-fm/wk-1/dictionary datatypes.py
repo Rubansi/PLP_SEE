da3b7014dd6980949c3cfa3b66e0d5ef45c30320
rubansi <rubansi24@gmail.com>
2025-08-02 19:54:40 +0300

    exploring dictionary datatypes in python

---
+++ python-fm/wk-1/dictionary datatypes.py
@@ -0,0 +1,8 @@
+# Python dictionary is an ordered collection of items. It stores elements in key/value pairs.
+
+# Here, keys are unique identifiers that are associated with each value.
+
+# Let's see an example,
+
+capital_city = {"Kenya": "Nairobi", "Nigeria": "Lagos"}
+print(capital_city)
