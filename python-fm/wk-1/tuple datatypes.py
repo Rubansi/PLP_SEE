b9e5c413af72dbc7c74c0fcdeb17517503a8bfeb
rubansi <rubansi24@gmail.com>
2025-08-02 19:44:11 +0300

    tuple datatypes in python

---
+++ python-fm/wk-1/tuple datatypes.py
@@ -0,0 +1,15 @@
+# A tuple is an ordered sequence of items same as a list. The only difference is that tuples are immutable. Tuples once created cannot be modified.
+
+# Tuples are defined using parentheses () instead of square brackets [].
+# Tuples can store items of different data types, including numbers, strings, and even lists.
+# In Python, we use the parentheses () to store items of a tuple. For example,
+
+products = ('XBox', 499.99, "Habibi", 23)
+print(products)
+
+# Access Tuple Items
+# To access items from a tuple, we use the index number (0, 1, 2 ...). For example,
+print(products[0]) #XBox
+print(products[1]) #499.99
+print(products[2]) #Habibi
+print(products[3]) #23
