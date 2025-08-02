02eb0000425ef85047c99a05410c9ce62b6620b3
rubansi <rubansi24@gmail.com>
2025-08-02 19:12:41 +0300

    assigning a new value to a variable

--- python-fm/wk-1/variables.py
+++ python-fm/wk-1/variables.py
@@ -2,3 +2,8 @@
 # variable name should be descriptive and follow naming conventions
 site_name = "Power Learn Project"
 print(site_name)
+
+# Assigning new value to site_name
+site_name = "I love coding 😊"
+print(site_name)
+
